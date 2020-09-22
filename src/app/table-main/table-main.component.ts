8cf4aea065ed185c90c4f06708b22d139a3862c5
Viktor Danilchenko <danilchenko.viktor@gmail.com>
2020-09-22 23:07:54 +0300

    Add services to taible-main

--- src/app/table-main/table-main.component.ts
+++ src/app/table-main/table-main.component.ts
@@ -1,41 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CalendarService } from '../Services/calendar.service';
+import { CatalogService } from '../Services/catalog.service';
+
+const COLUMN_HEADERS_MOCK = [
+  'Date', 'Sum spend', 'Rest', 'Sum per day',
+];
+
+type DashboarHeader = { title: string };
+
 @Component({
   selector: 'app-table-main',
   templateUrl: './table-main.component.html',
   styleUrls: ['./table-main.component.css']
 })
 export class TableMainComponent implements OnInit {
-  currentDateRow: Array<string> = [
-    'Fri May 01 ', 'Sat May 02 ', 'Sun May 03 ',
-    'Mon May 04 ', 'Tue May 05 ', 'Wed May 06 ',
-    'Thu May 07 ', 'Fri May 08 ', 'Sat May 09 ',
-    'Sun May 10 ', 'Mon May 11 ', 'Tue May 12 ',
-    'Wed May 13 ', 'Thu May 14 ', 'Fri May 15 ',
-    'Sat May 16 ', 'Sun May 17 ', 'Mon May 18 ',
-    'Tue May 19 ', 'Wed May 20 ', 'Thu May 21 ',
-    'Fri May 22 ', 'Sat May 23 ', 'Sun May 24 ',
-    'Mon May 25 ', 'Tue May 26 ', 'Wed May 27 ',
-    'Thu May 28 ', 'Fri May 29 ', 'Sat May 30 ',
-    'Sun May 31 '
-  ];
+  
+  constructor(
+    private calendarService: CalendarService,
+    private catalogService: CatalogService,
+  ) {}
+  
+  currentDateRow: Array<string> = this.calendarService.getCurrentMonth();
+
   year: number;
   month: number;
 
   columns: Array<any> = [];
 
-  columnsHeaders: Array<any> = [
-    { title: 'Date' },
-    { title: 'Sum spend' },
-    { title: 'ostatok' },
-    { title: 'Sum in day' },
-    { title: 'food' },
-    { title: 'alko' },
-  ];
+  categories: string[] = this.catalogService.getActiveCategories();
 
-  constructor() { }
+  columnsHeaders: DashboarHeader[] = this.getColumnHeaders();
 
   ngOnInit(): void {
+  }
+
+  getColumnHeaders(): DashboarHeader[] {
+    const headers = [...COLUMN_HEADERS_MOCK, ...this.categories];
+    return headers.map(el => ({ title: `${el}` }));
   }
 
   setCurrentYear(): number {
